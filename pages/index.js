01e5c924ac47496cbf064f264a4bc4bacb13c5df
yatin goyal <yatingoyal31@gmail.com>
2024-01-12 19:04:05 +0530

    added basic nextjs files

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+//localhost.com:3000/
+
+function HomePage(){
+    return <h1>The Home Page</h1>
+};
+
+export default HomePage;
---
+++ pages/news.js
@@ -0,0 +1,7 @@
+//localhost.com:3000/news
+
+function NewsPage(){
+    return <h1>The News Page</h1>
+};
+
+export default NewsPage;
